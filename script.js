ce6721862e19a0531985ac21fe88b20a4cd932c4
Martu Peluso <martupeluso@proton.me>
2025-11-16 18:31:41 -0300

    Create gameboard module with board initiation

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameboard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(0);
+    }
+  }
+
+  return {
+    board,
+  };
+})();
+
+console.log(gameboard.board);
