03f087a02bc595f251ad1ead5a96af6f0670fd8f
Martu Peluso <martupeluso@proton.me>
2025-11-18 20:54:40 -0300

    Change empty cell values from 0 to empty string
    Makes the empty state clearer and maintains consistency with "X" & "O"
    markers being strings

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gameboard = (function () {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push(0);
+      board[i].push("");
     }
   }
 
